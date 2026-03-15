576f3758f488eb3c2643ba2065fcecc5b00c3087
Huo Yibo <hybsfcbztk@outlook.com>
2026-03-15 23:27:25 +0800

    Update 1124双端队列-双链表和固定数组实现.cpp

--- 1124双端队列-双链表和固定数组实现.cpp
+++ 1124双端队列-双链表和固定数组实现.cpp
@@ -1,12 +1,103 @@
 #define _CRT_SECURE_NO_WARNINGS
+
+// è®¾è®¡å¾ªç¯åŒç«¯é˜Ÿåˆ—
+// æµ‹è¯•é“¾æ¥ : https://leetcode.cn/problems/design-circular-deque/
+
+class MyCircularDeque {
+private:
+    int size, capacity;
+    int l, r;
+    vector<int> deq;
+
+public:
+    MyCircularDeque(int k) {
+        size = 0;
+        l = r = 0;
+        capacity = k;
+        deq.resize(k, 0);
+    }
+
+    bool insertFront(int value) {
+        if (isFull())
+            return false;
+        if (isEmpty()) {
+            l = r = 0;
+        } else {
+            l = (l - 1 + capacity) % capacity;
+        }
+        deq[l] = value;
+        size++;
+        return true;
+    }
+
+    bool insertLast(int value) {
+        if (isFull())
+            return false;
+        if (isEmpty()) {
+            l = r = 0;
+        } else {
+            r = (r + 1) % capacity;
+        }
+        deq[r] = value;
+        size++;
+        return true;
+    }
+
+    bool deleteFront() {
+        if (isEmpty())
+            return false;
+        l = (l + 1) % capacity;
+        size--;
+        return true;
+    }
+
+    bool deleteLast() {
+        if (isEmpty())
+            return false;
+        r = (r - 1 + capacity) % capacity;
+        size--;
+        return true;
+    }
+
+    int getFront() {
+        if (isEmpty())
+            return -1;
+        return deq[l];
+    }
+
+    int getRear() {
+        if (isEmpty())
+            return -1;
+        return deq[r];
+    }
+
+    bool isEmpty() { return size == 0; }
+
+    bool isFull() { return size == capacity; }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
+
+
 #include <iostream>
 using namespace std;
-//Êı×éÊµÏÖË«¶Ë¶ÓÁĞ
+//æ•°ç»„å®ç°åŒç«¯é˜Ÿåˆ—
 #define MAXSIZE 100
 int* arr = new int[MAXSIZE];
 int left, right;
-int limit;//×î´ó´æ´¢ÊıÁ¿
-int size = 0;//¼ÇÂ¼µ±Ç°Êı×éÖĞÔªËØ¸öÊı
+int limit;//æœ€å¤§å­˜å‚¨æ•°é‡
+int size = 0;//è®°å½•å½“å‰æ•°ç»„ä¸­å…ƒç´ ä¸ªæ•°
 
 bool isEmpty()
 {
