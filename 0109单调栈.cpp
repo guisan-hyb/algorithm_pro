3d9d4c689e7a2fddf1f5078157340b7b4c294fbe
Huo Yibo <hybsfcbztk@outlook.com>
2026-04-10 22:07:07 +0800

    Update 0109单调栈.cpp

--- 0109单调栈.cpp
+++ 0109单调栈.cpp
@@ -3,6 +3,37 @@
 #include <algorithm>
 using namespace std;
 
+//附上灵神模板：
+pair<vector<int>, vector<int>> nearestGreater(vector<int>& nums) {
+    int n = nums.size();
+    // left[i] 是 nums[i] 左侧最近的严格大于 nums[i] 的数的下标，若不存在则为 -1
+    vector<int> left(n);
+    vector<int> st{-1}; // 哨兵
+    for (int i = 0; i < n; i++) {
+        int x = nums[i];
+        while (st.size() > 1 && nums[st.back()] <= x) { // 如果求严格小于，改成 >=
+            st.pop_back();
+        }
+        left[i] = st.back();
+        st.push_back(i);
+    }
+
+    // right[i] 是 nums[i] 右侧最近的严格大于 nums[i] 的数的下标，若不存在则为 n
+    vector<int> right(n);
+    st = {n}; // 哨兵
+    for (int i = n - 1; i >= 0; i--) {
+        int x = nums[i];
+        while (st.size() > 1 && nums[st.back()] <= x) { // 如果求严格小于，改成 >=
+            st.pop_back();
+        }
+        right[i] = st.back();
+        st.push_back(i);
+    }
+
+    return {left, right};
+}
+
+
 
 //单调栈--上
 //单调栈最经典的用法是解决如下问题：
